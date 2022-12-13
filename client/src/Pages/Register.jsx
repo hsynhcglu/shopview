0a3316bbdc820accc3cdf9d6fbd606880be98cef
huseyin <huseyinhacioglu55@icloud.com>
2022-12-13 13:29:51 +0300

    sweet alert some changes

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -4,8 +4,7 @@ import {AiOutlineEye, AiOutlineClose} from 'react-icons/ai'
 import {Link} from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux'
 import { signup } from '../actions/userActions.js'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import { useNavigate } from "react-router-dom";
 
 const Register = ({history}) => {
     const initialFormData = {
@@ -20,18 +19,8 @@ const Register = ({history}) => {
     const { error } = userState
     const [form, setForm] = useState(initialFormData)
     const dispatch = useDispatch()
-    const MySwal = withReactContent(Swal)
-    const Toast = MySwal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
+    const navigate = useNavigate()
+    
 
   return (
 
@@ -43,49 +32,46 @@ const Register = ({history}) => {
                 <h1 className='text-4xl text-txt-c1 font-bold text-center'>Shop<span className='text-txt-c2'>View</span></h1>
                 <form onSubmit={(e) => {
                         e.preventDefault()
-                            dispatch(signup(form, history))
-                            Toast.fire({
-                                icon: 'success',
-                                title: 'Kayıt İşlemi Başarıyla Tamamlanmıştır'
-                              })
-                            
+                        dispatch(signup(form, history))
+                        setTimeout(()=>{
+                            navigate('/')
+                        },300)
                     }}>
-                <div className='grid gap-y-4 mt-10'>
-                    <div>
-                        <h2 className='text-black font-medium'>First Name</h2>
-                        <input type='text' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your first name' onChange={(e) => setForm({...form, firstName: e.target.value})} />
-                    </div>
-                    <div>
-                        <h2 className='text-black font-medium'>Last Name</h2>
-                        <input type='text' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your last name' onChange={(e) => setForm({...form, lastName: e.target.value})} />
-                    </div>
-                    <div>
-                        <h2 className='text-black font-medium'>Email</h2>
-                        <input type='text' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your email' onChange={(e) => setForm({...form, email: e.target.value})} />
-                    </div>
-                    <div>
-                        <h2 className='text-black font-medium'>Password</h2>
-                        <div className='relative'>
-                            <input type='password' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your password' onChange={(e) => setForm({...form, password: e.target.value})} /> 
-                            <AiOutlineEye className='right-2 absolute top-3 cursor-pointer' />
+                    <div className='grid gap-y-4 mt-10'>
+                        <div>
+                            <h2 className='text-black font-medium'>First Name</h2>
+                            <input type='text' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your first name' onChange={(e) => setForm({...form, firstName: e.target.value})} />
+                        </div>
+                        <div>
+                            <h2 className='text-black font-medium'>Last Name</h2>
+                            <input type='text' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your last name' onChange={(e) => setForm({...form, lastName: e.target.value})} />
+                        </div>
+                        <div>
+                            <h2 className='text-black font-medium'>Email</h2>
+                            <input type='text' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your email' onChange={(e) => setForm({...form, email: e.target.value})} />
+                        </div>
+                        <div>
+                            <h2 className='text-black font-medium'>Password</h2>
+                            <div className='relative'>
+                                <input type='password' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your password' onChange={(e) => setForm({...form, password: e.target.value})} /> 
+                                <AiOutlineEye className='right-2 absolute top-3 cursor-pointer' />
+                            </div>
+                        </div>
+                        <div>
+                            <h2 className='text-black font-medium'>Confirm Password</h2>
+                            <div className='relative'>
+                                <input type='password' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your password' onChange={(e) => setForm({...form, confirmPassword: e.target.value})} /> 
+                                <AiOutlineEye className='right-2 absolute top-3 cursor-pointer' />
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <h2 className='text-black font-medium'>Confirm Password</h2>
-                        <div className='relative'>
-                            <input type='password' className='bg-gray-200 w-72 h-10 rounded-md pl-4 placeholder:text-gray-500 placeholder:text-sm' placeholder='Enter your password' onChange={(e) => setForm({...form, confirmPassword: e.target.value})} /> 
-                            <AiOutlineEye className='right-2 absolute top-3 cursor-pointer' />
-                        </div>
+                    <div className='mt-10'>
+                        <button type='submit' className='bg-txt-c2 text-white w-full h-10 text-center rounded-md'>Register</button>
+                        <h2 className='text-center font-bold text-base my-4'>Or</h2>
+                        <button  className='bg-txt-c1 text-white w-full h-10 text-center rounded-md'>Login</button>
+                        {error && <div>{error}</div>}
                     </div>
-                </div>
-                <div className='mt-10'>
-                    <button type='submit' className='bg-txt-c2 text-white w-full h-10 text-center rounded-md'>Register</button>
-                    <h2 className='text-center font-bold text-base my-4'>Or</h2>
-                    <button  className='bg-txt-c1 text-white w-full h-10 text-center rounded-md'>Login</button>
-                    {error && <div>{error}</div>}
-                </div>
                 </form>
-
             </div>
         </div>
     </div>
--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,18 +1,42 @@
-import { AUTH, ALERT } from '../constants/actionsConstants.js'
-import * as api from '../axios'
+import { AUTH, ALERT } from "../constants/actionsConstants.js";
+import * as api from "../axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+const Toast = MySwal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
 
 export const signup = (formData, history) => async (dispatch) => {
-    try {
-        const { data } = await api.signUp(formData)
+  try {
+    const { data } = await api.signUp(formData);
+    Toast.fire({
+      icon: "success",
+      title: "Kayıt İşlemi Başarıyla Tamamlanmıştır",
+    });
 
-        dispatch({type: AUTH, payload: data})
-
-    }catch(error) {
-        dispatch(
-            {type: ALERT, 
-                payload: 
-                error.response && error.response.data.message 
-                ? error.response.data.message 
-                : error.message})
-    }
-}
+    dispatch({ type: AUTH, payload: data });
+    
+  } catch (error) {
+    Toast.fire({
+      icon: "error",
+      title: error.response.data.message,
+    });
+    dispatch({
+      type: ALERT,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
